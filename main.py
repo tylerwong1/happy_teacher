952dd32f625331edaee49739aba6ec68aefa265c
Wong <tyler.wong@ufl.edu>
2023-01-31 16:58:20 -0500

    upload jupyter notebook

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import pandas as pd
+import numpy as np
 
+df = pd.read_csv('datatsets/dailyActivity_merged.csv')
+
+to_drop = ['LoggedActivitiesDistance']
+
+df.drop(to_drop, inplace=True, axis=1)
+
+print(df)
